afa0b548e0a2458622ba4ebbba167ab8793e988a
Errollinsjr <errollins.jr@gmail.com>
2021-09-03 13:25:50 -0400

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require('./routes');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 const app = express();
 
